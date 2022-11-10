8818d6a03d535135c3a0a6a0e0d304c6587aeb46
MarinaDuginova <duginovamarina@gmail.com>
2022-11-10 15:44:35 -0800

    US12

--- src/test/java/com/trycloud/runner/CukesRunner.java
+++ src/test/java/com/trycloud/runner/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/trycloud/step_definitions" ,
         dryRun = false ,
-        tags = "@US12" ,
+        tags = "@wip",
         publish = true
 )
 public class CukesRunner {
