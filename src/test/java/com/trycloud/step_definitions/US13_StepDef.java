e6e4e81002b71aa3020de71989b3358d9ad5eb23
Cydeo-Tetiana <dyopka@gmail.com>
2022-11-10 00:05:02 -0600

    us13 step def edited and done

--- src/test/java/com/trycloud/step_definitions/US13_StepDef.java
+++ src/test/java/com/trycloud/step_definitions/US13_StepDef.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebElement;
 
 public class US13_StepDef extends BasePage {
     US13_Page us13Page = new US13_Page();
-   // LoginPage loginPage = new LoginPage();
+
 
 
      /*
@@ -35,13 +35,15 @@ public class US13_StepDef extends BasePage {
     }
     @Then("verify the contact names are in the list")
     public void verify_the_contact_names_are_in_the_list() {
+
         BrowserUtils.waitFor(5);
         Assert.assertTrue(us13Page.contactList.size()>=2);
-      //   System.out.println(us13Page.contactList.size());
+
 
         for (WebElement each: us13Page.contactList){
+
             Assert.assertTrue(each.isDisplayed());
-            // System.out.println(each.getText());
+
         }
 
     }}
